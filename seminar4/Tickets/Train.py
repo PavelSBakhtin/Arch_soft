5f45a74a99a16e44d365249215bcd0d44c4b3201
PavelSB <1987PavelS@mail.ru>
2023-11-01 20:05:07 +0300

    home work 4 done

---
+++ seminar4/Tickets/Train.py
@@ -0,0 +1,13 @@
+class Train:
+    
+    def __init__(self):
+        self.numberOfSeats = None
+
+    def loadPassenger(self, ):
+        pass
+
+    def moveToLocation(self, ):
+        pass
+
+    def unloadPassenger(self, ):
+        pass
